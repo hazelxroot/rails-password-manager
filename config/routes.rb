237009508a3349aa8d2da6d0a0db0ed8b1750ff2
hazelxroot <samh349@gmail.com>
2025-01-11 19:42:26 -0500

    Add password sharing between users

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
-  devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  
-  resources :passwords
+  devise_for :users
+
+  resources :passwords do
+    resources :shares, only: [:new, :create, :destroy]
+  end
+
 
   # Defines the root path route ("/")
   root "passwords#index"
