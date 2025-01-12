6bc89d7ca0ce81ba94c9f7eceea08116876d4511
hazelxroot <samh349@gmail.com>
2025-01-11 21:02:09 -0500

    Add password sharing role & permissions

--- app/controllers/passwords_controller.rb
+++ app/controllers/passwords_controller.rb
@@ -1,6 +1,8 @@
 class PasswordsController < ApplicationController
   before_action :authenticate_user!
   before_action :set_password, except: %i[index new create]
+  before_action :require_editable_permission, only: %i[edit update]
+  before_action :require_deletable_permission, only: %i[destroy]
 
   def index
     @passwords = current_user.passwords
@@ -14,9 +16,10 @@ class PasswordsController < ApplicationController
   end
 
   def create
-    @password = current_user.passwords.create(password_params)
+    @password = Password.new(password_params)
+    @password.user_passwords.new(user: current_user, role: "owner")
 
-    if @password.persisted?
+    if @password.save
       redirect_to @password
     else
       render :new, status: :unprocessable_entity
@@ -47,5 +50,13 @@ class PasswordsController < ApplicationController
 
   def set_password
     @password = current_user.passwords.find(params[:id])
+  end
+
+  def require_editable_permission
+    redirect_to @password unless current_user_password.editable?
+  end
+
+  def require_deletable_permission
+    redirect_to @password unless current_user_password.deletable?
   end
 end
